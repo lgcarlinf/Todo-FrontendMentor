e69d61542271a4935b1ff0556221e755dd91d863
luiggy <usuario@MacBook-Air-de-Usuario.local>
2022-05-14 11:53:39 -0500

    s

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,14 +1,17 @@
-import {useState,useEffect, useContext} from "react";
-import AllNotes from "../components/AllNotes";
+import { useContext} from "react";
 import Header from "../components/Header";
 import NotesContext from "../context/NotesProvider";
+import desktopLight from  "../images/bg-desktop-light.jpg";
+import desktopDark from  "../images/bg-desktop-dark.jpg";
+import mobileDark from  "../images/bg-mobile-dark.jpg";
+import mobileLight from  "../images/bg-mobile-light.jpg";
 
 const Layout = ({children}) => {
  
   const {theme} = useContext(NotesContext);
   
   return (
-    <div className={`bg-[url('./images/bg-mobile-light.jpg')] md:bg-[url('./images/bg-desktop-light.jpg')] dark:bg-[url('./images/bg-mobile-dark.jpg')] md:dark:bg-[url('./images/bg-desktop-dark.jpg')] h-screen bg-no-repeat bg-[length:100%_220px] md:w-screen md:flex-col md:items-center ${theme=== 'dark' ? 'dark dark-scheme' : ''}`}>
+    <div className={`bg-[url(${mobileLight})] md:bg-[url(${desktopLight})] dark:bg-[url('./images/bg-mobile-dark.jpg')] md:dark:bg-[url('./images/bg-desktop-dark.jpg')] h-screen bg-no-repeat bg-[length:100%_220px] md:w-screen md:flex-col md:items-center ${theme=== 'dark' ? 'dark dark-scheme' : ''}`}>
       <Header />
     {children}
     </div>
